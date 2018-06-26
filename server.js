001df4b61937a0daacf31e58df85ca2fa83df5b6
gaelleiadvize <gaelle.acas@iadvize.com>
2018-06-26 10:36:16 +0200

    Do request

--- server.js
+++ server.js
@@ -6,27 +6,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-
-// First you need to create a connection to the db
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'girlscancode',
-});
-
-con.connect((err) => {
-    if(err){
-        console.log('Error connecting to Db');
-        return;
-    }
-    console.log('Connection established');
-});
-
-
-
-
-
-
 app.get('/', (req, res) => {
 
     let data = {
@@ -42,6 +21,64 @@ app.get('/', (req, res) => {
     res.send(content);
 });
 
+app.post('/', (req, res) => {
+
+    let data = {
+        'coucou' : 'COUCOU'
+    };
+
+    let  content = res.render('main', data);
+    // let content =  ejs.renderFile('main.ejs', [], [], function(err, str){
+    //      // str => Rendered HTML string
+    //  });
+
+
+    res.send(content);
+});
+
+
+
+
+
 app.listen(3000, function () {
+
+
+    // First you need to create a connection to the db
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'girlscancode',
+        database: 'movies'
+    });
+
+    con.connect((err) => {
+        if(err){
+            console.log('Error connecting to Db');
+            return;
+        }
+        console.log('Connection established');
+    });
+
+    con.query('SELECT * FROM movies', (err,rows) => {
+        if(err) throw err;
+
+        console.log('Data received from Db:\n');
+        console.log(rows);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     console.log('Go to http://localhost:3000 dans ton navigateur :) ')
 })
