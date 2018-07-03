8f0e9d2cc483be38cf94804019194d795286182c
Mayleen Lacouture <maylelacouture@gmail.com>
2018-07-03 21:40:33 +0200

    Add movies to database

--- config/movies.sql
+++ config/movies.sql
@@ -49,7 +49,15 @@ CREATE TABLE `movies` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `movies` (`id`, `title`, `year`, `favorites`)
+INSERT INTO `movies` (`id`, `title`, `year`, `picture`, `favorites`)
 VALUES
-  (1,'La grande vadrouille','1966',false),
-  (2,'Avengers','2012',true);
+  (1,'La grande vadrouille','1966','https://images-na.ssl-images-amazon.com/images/M/MV5BODc0NjVhZTktYjFiMy00YmRiLWJiZDAtODBkZDI2OTkwNjRjXkEyXkFqcGdeQXVyMjQ3NzUxOTM@._V1_SX300.jpg',false),
+  (2,'Avengers','2012','https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',false),
+  (3,'The Incredibles','2004','https://m.media-amazon.com/images/M/MV5BMTY5OTU0OTc2NV5BMl5BanBnXkFtZTcwMzU4MDcyMQ@@._V1_SX300.jpg',false),
+  (4,'Deadpool','2016','https://m.media-amazon.com/images/M/MV5BYzE5MjY1ZDgtMTkyNC00MTMyLThhMjAtZGI5OTE1NzFlZGJjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',false),
+  (5,'Black Panther','2018','https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_SX300.jpg',false),
+  (6,'Ant-Man','2015','https://ia.media-imdb.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_SX300.jpg',false),
+  (7,'Thor','2011','https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',false),
+  (8,'Jurassic Park','1993','https://ia.media-imdb.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg',false),
+  (9,'Blade Runner','1982','https://ia.media-imdb.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',false),
+  (10,'X-Men','2000','https://ia.media-imdb.com/images/M/MV5BZmJkOGY4YjYtM2FmYy00MTIyLTg2YTUtMzJiNjljMWM5MGUwXkEyXkFqcGdeQXVyNjExODE1MDc@._V1_SX300.jpg',false);
